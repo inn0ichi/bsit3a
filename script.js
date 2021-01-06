1e99d05d1944b3269cf67877bacec54742dea337
Meowieeee <reginemanuelstudentacc@gmail.com>
2021-01-06 17:06:29 +0800

    section details

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-function search() {
-    var input = document.getElementById("Search");
-    var filter = input.value.toLowerCase();
-    var nodes = document.getElementsByClassName('column');
 
-    for (i = 0; i < nodes.length; i++) {
-        if (nodes[i].innerText.toLowerCase().includes(filter)) {
-            nodes[i].style.display = "block";
-        } else {
-            nodes[i].style.display = "none";
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("add_section");
+    var span = document.getElementsByClassName("close")[0];
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
         }
     }
-}
+
+    function search() {
+        var input = document.getElementById("Search");
+        var filter = input.value.toLowerCase();
+        var nodes = document.getElementsByClassName('column');
+    
+        for (i = 0; i < nodes.length; i++) {
+            if (nodes[i].innerText.toLowerCase().includes(filter)) {
+                nodes[i].style.display = "block";
+            } else {
+                nodes[i].style.display = "none";
+            }
+        }
+    
+    }
+
